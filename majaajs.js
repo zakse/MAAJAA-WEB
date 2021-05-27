8a62b9061a7aae6545af71e3194c72f722424d41
zakse <53636837+zakse@users.noreply.github.com>
2021-05-27 16:04:30 +0300

    IMG update

--- majaajs.js
+++ majaajs.js
@@ -1,1 +1,23 @@
+//single line comment//
 
+/* multiple lines */
+
+var elements=document.getElementsByClassName("togglable");
+
+Array.from(elements).forEach(function(element) {
+    element.addEventListener('click', changeImage);
+  });
+
+function changeImage(event) {
+
+console.log(event);
+
+    if (document.getElementById("imgClickAndChange").src == "IMG/Privātmājas rekonstrukcija.webp") 
+    {
+        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+    }
+    else 
+    {
+        document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+    }
+}
